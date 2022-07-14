6b93316ea44cebad877699fb33d3730b3d1e74c4
Suraj Subramanian <5676233+suraj813@users.noreply.github.com>
2022-07-14 14:28:49 -0400

    fix import

--- apps/charnn/model_fsdp.py
+++ apps/charnn/model_fsdp.py
@@ -23,7 +23,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from torch.distributed.fsdp.wrap import wrap
-from torch.distributed.algorithms._checkpoint._checkpoint_wrapper import checkpoint_wrapper
+from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import checkpoint_wrapper
 from model import GPTConfig, MultiheadAttentionLayer, EmbeddingStem
 
 logger = logging.getLogger(__name__)
